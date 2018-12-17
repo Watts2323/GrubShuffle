605333bd9662f85843f18bd97083735b8a52235d
Xavier <xwatts2323@Xaviers-MacBook-Pro.local>
2018-12-17 16:07:21 -0700

    Initial commit

---
+++ GrubShuffle/OnBoardingPageViewController.swift
@@ -0,0 +1,33 @@
+//
+//  OnBoardingPageViewController.swift
+//  GrubShuffle
+//
+//  Created by Xavier on 12/17/18.
+//  Copyright © 2018 Xavier ios dev. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingPageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.dataSource = self
+        self.delegate = self
+        
+        if let ifrs
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
